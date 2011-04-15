d57d43529685d86b1c5bfd131418f90fad71241a
Harley <harley@bigdoor.com>
2011-04-14 21:40:35 -0700

    Update package iwth repository URL

--- package.js
+++ package.js
@@ -1,2 +1,15 @@
-{ "name": "restedjs",
+{
+	"name": "restedjs",
+	"description": "RESTful microframework for Express",
+	"main": "./rested.js",
+	"author": "Harley Holt",
+	"version": "0.1",
+	"repository": {
+		"type": "git",
+		"url": "https://github.com/harleyholt/restedjs"
+	},
+	"bugs": {
+		"web": "https://github.com/harleyholt/restedjs/issues" 
+	}
+}
 
