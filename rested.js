d0667d4dbf445abb155f60b59752a436701397d9
Harley <harley@bigdoor.com>
2011-04-20 00:42:49 -0700

    Working implementation of passthrough

--- rested.js
+++ rested.js
@@ -2,24 +2,29 @@ var methods = ['get', 'post', 'put', 'delete'];
 function rested(app, path, controller) {
 	//TODO: this only works for string paths, will fail if a regex was used
 
-	// setup the passthough if it exists
+	// setup the passthough if it exists -- all requests which partially
+	// match the path will go through the passthrough so that it can
+	// perform whatever work it wishes
 	if ( controller.passthrough ) {
-		app.all(path, controller.passthrough);
-
-		if ( /^.*\?$/(path) ) {
-			path = path.substring(0, path.length - 1);
+		var _path = path;
+		if ( /^.*\?$/(_path) ) {
+			_path = _path.substring(0, path.length - 1);
 		}
-		app.all(path+'/*', controller.passthrough);
+		if ( /^.*\/$/(_path) ) {
+			_path = _path.substring(0, path.length - 1);
+		}
+		app.all(_path+'/*', controller.passthrough);
 	}
 
 	for ( var i = 0; i < methods.length; i++ ) {
 		if ( controller[methods[i]] ) {
-			app[methods[i]]( 
-				path,
-				(function(method) { 
-					return controller[method]; 
-				})(methods[i])
-			);
+			var args = []
+			args.push(path);
+			if ( controller.passthrough ) {
+				args.push(controller.passthrough);
+			}
+			args.push(controller[methods[i]]);
+			app[methods[i]].apply(app, args);
 		}
 	}
 }
