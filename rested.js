82d809a807b2f18cb18fe6e1d60e3d967d3de454
Harley <harley@bigdoor.com>
2011-04-26 16:28:17 -0700

    minor refactoring

--- rested.js
+++ rested.js
@@ -1,30 +1,15 @@
 var methods = ['get', 'post', 'put', 'delete'];
 
 function authorize(handler, on_error, req, res, next) {
-	if ( handler.authorize ) {
-		if ( !handler.authorize(req) ) {
-			return;
-		}
+	if ( handler.authorize && !handler.authorize(req) ) {
+		return;
 	} 
 	handler(req, res, next);
 }
 
 function rested(app, path, controller) {
 	//TODO: this only works for string paths, will fail if a regex was used
-
-	// setup the passthough if it exists -- all requests which partially
-	// match the path will go through the passthrough so that it can
-	// perform whatever work it wishes
-	if ( controller.passthrough ) {
-		var _path = path;
-		if ( /^.*\?$/(_path) ) {
-			_path = _path.substring(0, path.length - 1);
-		}
-		if ( /^.*\/$/(_path) ) {
-			_path = _path.substring(0, path.length - 1);
-		}
-		app.all(_path+'/*', controller.passthrough);
-	}
+	//
 
 	for ( var i = 0; i < methods.length; i++ ) {
 		if ( controller[methods[i]] ) {
@@ -44,6 +29,21 @@ function rested(app, path, controller) {
 			app[methods[i]].apply(app, args);
 		}
 	}
+
+	// setup the passthough if it exists -- all requests which partially
+	// match the path will go through the passthrough so that it can
+	// perform whatever work it wishes
+	if ( controller.passthrough ) {
+		var _path = path;
+		if ( /^.*\?$/(_path) ) {
+			_path = _path.substring(0, path.length - 1);
+		}
+		if ( /^.*\/$/(_path) ) {
+			_path = _path.substring(0, path.length - 1);
+		}
+		app.all(_path+'/*', controller.passthrough);
+	}
+
 }
 
 module.exports = rested;
